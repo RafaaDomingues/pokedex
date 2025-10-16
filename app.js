5ef2c0a69be3b55ec936ec775856d2575892b242
RafaaDomingues <rafaellycaroline199@gmail.com>
2025-10-16 12:15:35 -0300

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+const pokemon_number = document.getElementById("pknum");
+const pokemon_nome = document.getElementById("pkname");
+const pokemon_img = document.getElementById("pking");
+
+const pokemon_form = document.getElementById("form");
+const pokemon_input = document.getElementById("input");
+
+const pokemon_prev = document.getElementById("bnt-prev");
+const pokemon_next = document.getElementById("bnt-next");
+
