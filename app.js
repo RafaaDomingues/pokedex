78fd6412d5de89a44337a82b57321b062ef3c262
Nilson-DataScience <nilsongithub@gmail.com>
2025-11-18 09:11:05 -0300

    Refactor Pokémon app for improved readability

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
 const pokemon_number = document.getElementById("pknum");
 const pokemon_name = document.getElementById("pkname");
-const pokemon_img = document.getElementById("pking");
+const pokemon_img = document.getElementById("pkimg");
 
 const pokemon_form = document.getElementById("form");
 const pokemon_input = document.getElementById("input");
 
-const pokemon_prev = document.getElementById("bnt-prev");
-const pokemon_next = document.getElementById("bnt-next");
+const pokemon_prev = document.getElementById("bnt-prev")
+const pokemon_next = document.getElementById("bnt-next")
 
 let searchPokemon = 1;
 
+
 const fetchpokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    if (APIResponse.status === 200) {
+    const APIResponse = await fetch
+    (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if(APIResponse.status ===200){
         const data = await APIResponse.json();
         return data;
+
     }
+    
 }
 
-const renderPokemon = async (pokemon) => {
-    const data = await fetchpokemon(pokemon)
-    if (data) {
+const renderPokemon = async (pokemon)=> {
+    const data = await fetchpokemon(pokemon);
+    if(data){
         pokemon_name.innerHTML = data.name;
         pokemon_number.innerHTML = data.id;
-        pokemon_img.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    } else {
-        pokemon_name.innerHTML = 'Não encontrado';
-        pokemon_number.innerHTML = '';
-        pokemon_img.style.display = "none";
+        pokemon_img.src= data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    } else{
+        pokemon_name.innerHTML = 'Não Encontrado';
+        pokemon_number.innerHTML = ''
+        pokemon_img.style.display= 'none'
+
+}
+
     }
+   
 
-} 
-
-pokemon_form.addEventListener("submit", (event) => {
+pokemon_form.addEventListener("submit", (event)=>{
     event.preventDefault();
-    console.log("Enviando Formulario", pokemon_input.value);
+    console.log("Enviando Formulario",pokemon_input.value);
     renderPokemon(pokemon_input.value.toLowerCase());
-    InputDeviceInfo.value = "";
+    input.value = "";
+
 })
 
 pokemon_prev.addEventListener("click", ()=>{
     if(searchPokemon > 1){
-        searchPokemon-=1
+        searchPokemon -=1 
         renderPokemon(searchPokemon)
     }
+   
+
 })
 
 pokemon_next.addEventListener("click", ()=>{
-    searchPokemon += 1;
-    renderPokemon(searchPokemon)
+  searchPokemon +=1 
+   renderPokemon(searchPokemon)
 })
+
+
 
 renderPokemon(searchPokemon)
