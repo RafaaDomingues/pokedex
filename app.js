a84cd243b322693d7e0b36b4af0144c051a2f025
Rafaelly Domingues <rafaellycaroline199@gmail.com>
2025-11-18 09:16:46 -0300

    update

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ pokemon_form.addEventListener("submit", (event)=>{
     event.preventDefault();
     console.log("Enviando Formulario",pokemon_input.value);
     renderPokemon(pokemon_input.value.toLowerCase());
+    searchPokemon = parseInt(pokemon_input.value);
     input.value = "";
 
 })
